a79fd3ebadbe5f5808661538dfc160ca8ba50b02
akulaj9 <jenianik29@gmail.com>
2017-11-09 00:25:32 +0200

    initial commit

---
+++ task1_6.py
@@ -0,0 +1,55 @@
+print("===============================")
+print("Task #1")
+print("===============================")
+
+a = 10
+b = 25
+c = 30
+equation = a + b * (c / 2)
+result = "Выражение a + b * (c / 2) при a = %d, b = %d, c = %d, равно %d." % (a, b, c, equation)
+print(result)
+
+
+print("===============================")
+print("Task #2")
+print("===============================")
+
+a = 10
+b = 5
+equation = (a**2 + b**2) % 2
+result = "Выражение №2 при a = %d, b = %d, равно %d." % (a,b, equation)
+print(result)
+
+
+print("===============================")
+print("Task #3")
+print("===============================")
+
+a = 10
+b = 20
+c = 30
+equation = (a + b) / 12 * c % 4 + b
+result = "При a = %d, b = %d, c = %d, b = %d, равно %d." % (a, b, c, b, equation)
+print(result)
+
+
+print("===============================")
+print("Task #4")
+print("===============================")
+
+a = 15
+b = 26
+c = 35
+equation = (a - b * c) / (a + b) % c
+result = "При a = %d, b = %d, c = %d, a = %d, b = %d, c = %d, равно %d." % (a, b, c, a, b, c, equation)
+print(result)
+
+
+print("===============================")
+print("Task #5")
+print("===============================")
+
+
+print("===============================")
+print("Task #6")
+print("===============================")
