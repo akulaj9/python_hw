1e6d6b1f6192aa39de4de7735cddffa5e1070f45
akulaj9 <jenianik29@gmail.com>
2017-11-18 12:39:21 +0200

    initial commit

--- task7_10.py
+++ task7_10.py
@@ -3,7 +3,6 @@ print("Task #7")
 print("===============================")
 
 current_date = "11.14.2017"
-
 print(current_date)
 lst = current_date.split(".")
 mounth = lst[0]
@@ -24,3 +23,33 @@ name = lst[0]
 surname = lst[1]
 student_new = surname + " " + name
 print(student_new)
+
+print("===============================")
+print("Task #9")
+print("===============================")
+
+string = "employee_first_name"
+print(string)
+lst = string.split("_")
+employee = lst[0]
+first = lst[1]
+name = lst[2]
+result = employee.title() + first.title() + name.title()
+print(result)
+
+
+print("===============================")
+print("Task #10")
+print("===============================")
+
+s = 'Leo Tolstoy*1828-08-28*1910-11-20'
+lst = s.split('*')
+print(lst)
+name = s.split( "*")[0]
+date1 = lst[1]
+date2 = lst[2]
+year1 = date1.split('-')[0]
+year2 = date2.split('-')[0]
+age = int(year2) - int(year1)
+result = "%s, %d" % (name, age)
+print(result)
