3d3ed3e74591ecfdd58116c0a92dac699ea245d1
akulaj9 <jenianik29@gmail.com>
2017-11-29 22:30:18 +0200

    initial commit

---
+++ tests.py
@@ -0,0 +1,28 @@
+print("===============================")
+print("Task 1_3")
+print("===============================")
+
+def example1(a, b, c):
+    import math
+    return (a+b*c)**2
+
+def example2(a, b, c):
+    import math
+    return a-4*b/c
+
+def example3(a, b, c):
+    import math
+    return (a*b+4)/(c-1)
+
+a = 1
+b = 2
+c = 3
+result1 = example1(a, b, c)
+result2 = example2(a, b, c)
+result3 = example3(a, b, c)
+
+power2 = "\u00B2"
+
+print("For example (a+b*c)%s the answer = %0.2f" % (power2, result1))
+print("For example a-4*b/c the answer = %0.2f" % result2)
+print("For example (a*b+4)/(c-1) the answer = %0.2f" % result3)
